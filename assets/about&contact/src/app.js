bc5d28332a168371e44385081aae5d99f7a60254
zf-2022 <162259676+HWRacingDubai@users.noreply.github.com>
2024-08-17 11:57:55 +0400

    re-organising files, just moved places and set ocming soo page. Not matched other links yet

--- assets/about&contact/src/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    if ($(Document.window).width() > 1300) {
-        $("#include").load("/src/about.html");
-    } else {
-        $("#include").load("src/comingSoon/comingSoon.html");
-    }
-});
